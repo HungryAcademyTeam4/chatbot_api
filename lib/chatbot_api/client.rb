b70c62b5b21e10f49f32d7aee16c912db2fd5e4f
michael verdi <michael.v.verdi@gmail.com>
2012-06-05 15:45:04 -0400

    it works

--- lib/chatbot_api/client.rb
+++ lib/chatbot_api/client.rb
@@ -3,23 +3,29 @@ require 'faraday'
 
 module ChatbotApi
   class Client
-    BASE_URL = "http://localhost:3000/api/v1"
+    BASE_URL = "http://localhost:3000"
 
     def initialize
       @connection = Faraday.new(url: BASE_URL)
     end
 
     def get_all_chat_rooms
-      response = @connection.get('/chat_rooms')
+      @connection.get do |req|
+        req.url "/api/v1/chat_rooms"
+        req.headers['Content-Type'] = 'application/json'
+      end
     end
 
     def get_chat_room_by_id(id)
-      response = @connection.get("/chat_rooms/#{id}")
+      @connection.get do |req|
+        req.url "/api/v1/chat_rooms/#{id}"
+        req.headers['Content-Type'] = 'application/json'
+      end
     end
 
     def create_chat_room(attributes)
       @connection.post do |req|
-        req.url '/chat_rooms'
+        req.url "/api/v1/chat_rooms"
         req.headers['Content-Type'] = 'application/json'
         req.body = attributes
       end
